f2131998f514859f9f1a6aebd1b1ffc65408303b
Johanes Neumann <johanes.neumann@gmail.com>
2024-07-06 17:19:33 -0300

    Commit  inicial

---
+++ src/main/java/com/caju/card/authorization/CardTransactionAuthorizationApplication.java
@@ -0,0 +1,13 @@
+package com.caju.card.authorization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardTransactionAuthorizationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardTransactionAuthorizationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/caju/card/authorization/CardTransactionAuthorizationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caju.card.authorization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardTransactionAuthorizationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
